f9d6d33c378b98c5f897cc990f7eb60a79f37624
unknown <opportunities91@gmail.com>
2025-01-13 15:43:17 -0800

    project update

---
+++ README.md
@@ -0,0 +1,33 @@
+Simple Todo App
+A sleek and efficient app to help you keep track of your tasks and to-dos.
+
+Functionality
+The Simple Todo App allows users to:
+
+Add new to-do items with optional due dates.
+
+Mark tasks as completed.
+
+View a list of all tasks, both completed and pending.
+
+Delete tasks that are no longer needed.
+
+Optionally validate input fields to ensure data integrity.
+
+Technology
+This project leverages the following technologies:
+
+HTML: For structuring the webpage and the to-do list elements.
+
+CSS: For styling the application, ensuring a clean and user-friendly interface.
+
+JavaScript (ES6): For interactive features, such as adding, validating, and deleting tasks.
+
+UUID: For generating unique IDs for each task.
+
+Modules: Organizing code into reusable components (Todo, FormValidator, etc.).
+
+Deployment
+This project is deployed on GitHub Pages:
+
+Simple Todo App Deployment Link
---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+import { initialTodos, validationConfig } from "../utils/constants.js";
+import Todo from "../components/Todo.js";
+import FormValidator from "../components/FormValidator.js";
+
+const addTodoButton = document.querySelector(".button_action_add");
+const addTodoPopup = document.querySelector("#add-todo-popup");
+const addTodoForm = addTodoPopup.querySelector(".popup__form");
+const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+const todosList = document.querySelector(".todos__list");
+
+const openModal = (modal) => {
+  modal.classList.add("popup_visible");
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove("popup_visible");
+};
+
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  const todoElement = todo.getView();
+  return todoElement;
+};
+
+addTodoButton.addEventListener("click", () => {
+  openModal(addTodoPopup);
+});
+
+addTodoCloseBtn.addEventListener("click", () => {
+  closeModal(addTodoPopup);
+});
+
+addTodoForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const name = evt.target.name.value;
+  const dateInput = evt.target.date.value;
+
+  const date = new Date(dateInput);
+  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+  const id = uuidv4();
+  const values = { name, date, id };
+  const todo = generateTodo(values);
+  todosList.append(todo);
+  closeModal(addTodoPopup);
+});
+
+initialTodos.forEach((item) => {
+  const todo = generateTodo(item);
+  todosList.append(todo);
+});
+
+const addTodoFormValidator = new FormValidator(validationConfig, addTodoForm);
+addTodoFormValidator.enableValidation();
